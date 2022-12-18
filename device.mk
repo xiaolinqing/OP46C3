eb39e71b021dda62a86043528a10596e77a0ab15
xiaolinqing <87248741+xiaolinqing@users.noreply.github.com>
2022-12-18 10:20:24 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,6 +15,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Inherit from OEM SOC-common
+$(call inherit-product, $(COMMON_PATH)/common.mk)
+
+# Copy modules for depmod
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
 
 LOCAL_PATH := device/oppo/OP46C3
 
