12410dad6cdd49f406bac826d39cd1d1a05b1d94
xiaolinqing <87248741+xiaolinqing@users.noreply.github.com>
2022-12-18 10:16:40 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+-include $(COMMON_PATH)/BoardConfigCommon.mk
 
 DEVICE_PATH := device/oppo/OP46C3
 
